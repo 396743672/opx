85bc448f9f3f83375566aa790b6a3cdb6ddde55a
Runic <runic.flow@outlook.com>
2026-09-15 12:36:49 +0800

    style(alert): 统一 system_monitor 模块声明与 re-export 排序

--- src-tauri/src/services/system_monitor/mod.rs
+++ src-tauri/src/services/system_monitor/mod.rs
@@ -1,8 +1,8 @@
-pub mod info;
-pub mod history;
 pub mod alerts;
-pub mod recorder;
+pub mod history;
+pub mod info;
 pub mod notify;
+pub mod recorder;
 
-pub use self::info::*;
 pub use self::history::*;
+pub use self::info::*;
